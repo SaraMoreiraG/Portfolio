02b00b03445e20dd4a401c1de18777c7c55523cc
SaraMoreiraG <sara_moreira@hotmail.es>
2023-12-17 19:05:05 +0100

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { I18nextProvider, useTranslation } from "react-i18next";
+import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
